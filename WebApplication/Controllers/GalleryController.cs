2b25cb671250eee6a81c351843a0cd6f2a8a1405
Michael <boulevardoffakes@gmail.com>
2017-11-18 15:21:34 +0500

    Changed theme, fixed routing

---
+++ WebApplication/Controllers/GalleryController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly GalleryModel gallery = new GalleryModel
+        {
+            MaxImages = 6,
+            Images = new[]
+            {
+                "/Images/1.jpg",
+                "/Images/2.jpg",
+                "/Images/3.jpg",
+                "/Images/4.jpg",
+                "/Images/5.jpg",
+                "/Images/6.jpg",
+            },
+            ImagePreviews = new[]
+            {
+                "/Images/1_thumbnail.jpg",
+                "/Images/2_thumbnail.jpg",
+                "/Images/3_thumbnail.jpg",
+                "/Images/4_thumbnail.jpg",
+                "/Images/5_thumbnail.jpg",
+                "/Images/6_thumbnail.jpg",
+            }
+        };
+
+        public ActionResult Index()
+        {
+            return View(gallery);
+        }
+    }
+}
